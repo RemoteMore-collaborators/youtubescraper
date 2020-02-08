e79a28d1b86cd398f78f1647cea77bbb56ccb4ef
Bryan-Cee <cheruiyotbryan@gmail.com>
2020-02-08 09:44:34 +0300

    optimize memory usage

--- scrape.py
+++ scrape.py
@@ -1,6 +1,5 @@
 import csv
 import time
-import memory_profiler
 
 from oauth2client.service_account import ServiceAccountCredentials
 from bs4 import BeautifulSoup
@@ -8,6 +7,8 @@ from datetime import datetime
 import gspread
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -15,14 +16,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 
 from utils import custom_logger, paste_csv_to_wks, is_in_english
-m1 = memory_profiler.memory_usage()
 t1 = datetime.now()
 
-# CURRENT_DIR = '/home/ubuntu/youtubescraper'
-CURRENT_DIR = '.'
+CURRENT_DIR = '/home/ubuntu/youtubescraper'
+#CURRENT_DIR = '.'
+DRIVER_PATH = '/usr/bin'
+#DRIVER_PATH = '.'
 WINDOW_SIZE = "1920,1440"
 URL = "https://www.youtube.com/user/CandyCrushOfficial/community"
-SCROLL_PAUSE_TIME = 5
+SCROLL_PAUSE_TIME = 3
+POST_TO_SCRAPE = 30
 
 current_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 filename = f'{CURRENT_DIR}/logs/scraped_{current_time}.log'
@@ -35,13 +38,13 @@ logger.info(f'Logfile name {filename}')
 chrome_options = Options()
 
 chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument('--disable-dev-shm-usage')
+#chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--disable-notifications")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument("--headless")
 
-driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
+driver = webdriver.Chrome(executable_path=f'{DRIVER_PATH}/chromedriver', options=chrome_options)
 
 driver.get(URL)
 
@@ -74,74 +77,62 @@ while True:
 
 all_posts = driver.find_elements_by_xpath('//*[@id="more"]/div/paper-button')
 total_number_of_posts = len(all_posts)
+all_posts = all_posts[:POST_TO_SCRAPE]
+total_post_to_scrape = len(all_posts)
 
 logger.info(f'Total number of posts {total_number_of_posts}')
+logger.info(f'Total number of posts to be scraped {total_post_to_scrape}')
 
 logger.info("Clicking through the comments section...")
 # Select all view comments and click them
-for x in range(total_number_of_posts):
+for x in range(POST_TO_SCRAPE):
     actions = ActionChains(driver)
     element = all_posts[x]
     logger.info(f"Clicked view comments in post {x + 1}")
     actions.move_to_element(element).click().perform()
-    time.sleep(3) 
+    time.sleep(3)
 
 logger.info("Clicking through the comments section done!")
 
-try:
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-except Exception as err:
-    logger.error(err)
-    exit()
-
 logger.info("Retrieving comments...")
-
-
-def parse_posts(soup):
-    post_block = len(soup.select('#contents > ytd-backstage-post-thread-renderer'))
-
-    for i in range(post_block):
-        post = soup.select(f'#contents > ytd-backstage-post-thread-renderer:nth-child({i + 1})')
-        post_text = post[0].select('yt-formatted-string#content-text.style-scope.ytd-backstage-post-renderer')[0].text.strip()
-        post_publish_time = post[0].select('yt-formatted-string#published-time-text > a')[0]
-        post_publish_time_text = post_publish_time.text
-        post_link = f'https://youtube.com{post_publish_time.get_attribute_list("href")[0]}'
-        post_body = soup.select('div#main.style-scope.ytd-backstage-post-renderer')
-        post_likes = post_body[i].select('span#vote-count-middle.style-scope.ytd-comment-action-buttons-renderer')[0].text.strip()
-        comment = post[0].select('#loaded-comments')[0].select('#main')
-        video = post[0].select('#content-attachment > ytd-video-renderer')
-        video_link = "No video"
-
-        if len(video) > 0:
-            video_link_markup = video[0].select("#thumbnail")
-            video_link = f'https://youtube.com{video_link_markup[0].get_attribute_list("href")[0]}'
-
-        video_and_comment = [post_text, post_publish_time_text, post_link, post_likes, video_link, comment]
-        yield(video_and_comment)
-
-posts = parse_posts(soup)
-
-# Getting all the comments and videos
 
 csv_written_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 filepath = f'{CURRENT_DIR}/csv/scraped_{csv_written_time}.csv'
 
 logger.info(f'Writing comments data to {filepath}...')
 
-with open(filepath, 'w', encoding='utf-8') as csv_file:
+with open(filepath, 'w+', encoding='utf-8') as csv_file:
     fileWriter = csv.writer(csv_file, dialect='excel')
-    
-    for post in posts:
-        post_text = post[0]
-        post_publish_time = post[1]
-        post_link = post[2]
-        post_likes = post[3]
-        video_link = post[-2]
-        posted_comments = post[-1]
+
+    for i in range(POST_TO_SCRAPE):
+        try:
+            post_block_ = driver.find_element_by_xpath(f'(//*[@id="contents"]/ytd-backstage-post-thread-renderer)[{i + 1}]')
+            post_block_ = post_block_.get_attribute('innerHTML')
+        except:
+            logger.error(f'Skipped post {i + 1}')
+            continue
+
+        soup = BeautifulSoup(post_block_, 'html.parser')
+
+        post_text = soup.select('yt-formatted-string#content-text.style-scope.ytd-backstage-post-renderer')[0].text
+        post_text = " ".join(post_text.split())
+        post_publish_time = soup.select('yt-formatted-string#published-time-text > a')[0]
+        post_publish_time_text = post_publish_time.text
+        post_link = f'https://youtube.com{post_publish_time.get_attribute_list("href")[0]}'
+        post_likes = soup.select('span#vote-count-middle.style-scope.ytd-comment-action-buttons-renderer')[0].text.strip()
+        posted_comments = soup.select('#loaded-comments')[0].select('#main')
+        video = soup.select('#content-attachment > ytd-video-renderer')
+
+        video_link = "No video"
+
+        if len(video) > 0:
+            video_link_markup = video[0].select("#thumbnail")
+            video_link = f'https://youtube.com{video_link_markup[0].get_attribute_list("href")[0]}'
+        
         n = len(posted_comments)
 
-        row = ['Candy Crush Saga Official', post_text, post_publish_time, post_likes, post_link]
+        row = ['Candy Crush Saga Official', post_text, post_publish_time_text, post_likes, post_link]
+
         fileWriter.writerow(row)
 
         if n > 0:
@@ -149,7 +140,6 @@ with open(filepath, 'w', encoding='utf-8') as csv_file:
                 comment_text = posted_comments[k].select('#content-text')[0].text.strip()
 
                 if not is_in_english(comment_text):
-                    logger.info(f"'{comment_text}' is not english")
                     continue
 
                 author = posted_comments[k].select('#author-text > span')[0].text.strip()
@@ -160,6 +150,10 @@ with open(filepath, 'w', encoding='utf-8') as csv_file:
                 fileWriter.writerow(row)
 
         fileWriter.writerow(['', '', '', '', ''])
+        logger.info(f"Parsed post {i + 1}")
+        
+        time.sleep(1)
+
 
 logger.info("Retrieving comments complete!")
 
@@ -177,9 +171,7 @@ paste_csv_to_wks(filepath, wks, 'A2', logger)
 logger.info('Writing to googlesheets complete!')
 
 t2 = datetime.now()
-m2 = memory_profiler.memory_usage()
 
 time_diff = t2 - t1
-mem_diff = m2[0] - m1[0]
-logger.info(
-    f"It took {time_diff} Secs and {mem_diff} Mb to execute this method")
+logger.info(f"It took {time_diff} Secs to execute this method")
+driver.quit()
